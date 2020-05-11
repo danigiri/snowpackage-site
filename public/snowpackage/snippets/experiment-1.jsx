10618c4da3adab1cf2ad9d5331a28442298454b2
Daniel Giribet <dani@calidos.cat>
2020-05-12 00:15:57 +0200

    first snippets

---
+++ public/snowpackage/snippets/experiment-1.jsx
@@ -0,0 +1,4 @@
+<Experiment experimentID="{1}">
+	<Search searchButtonCopy="Search!" searchExamplesCopy="iPhone, Android, phones..." startCategory="Phones"/>
+	<Search searchButtonCopy="Search!" searchExamplesCopy="Android, iPhone, phones..." startCategory="Phones"/>
+</Experiment>
